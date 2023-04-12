eff945f679b816fbf95e6a7900642ac9ee845263
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-12 12:26:51 -0600

    Say hi message added

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,26 @@ import MainGrid from "./components/MainGrid"
 import Nav from "./components/Nav"
 import Projects from "./components/Projects"
 import Skills from "./components/Skills"
+import arrow from "./assets/arrow.svg"
+
 
 function App() {
   return (
     <div className="overflow-hidden">
+      <div 
+        className="
+          hidden absolute right-14 z-[-1]
+          md:block md:top-[80px]
+        ">
+        <img width="138" src={arrow} alt="pointing line" />
+        <h3 className="text-2xl font-Tilt absolute left-[-50px] top-20">Say hi! 👋🏽</h3>
+      </div>
       <Nav />
       <MainGrid>
         <Banner />
         <Projects />
         <Skills />
-        <Forms />
+        <Forms /> 
         <Footer />
       </MainGrid>
     </div>
