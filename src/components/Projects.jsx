346e51b8d76a309779aceaefecbbecdf481b8ed8
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-11 18:34:22 -0600

    Added dummy projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,18 @@
 import Grid2x2 from "./Grid2x2"
 import Header2 from "./Header2"
+import ProjectCard from "./ProjectCard"
+import image1 from "../assets/dummy-project-1.jpg"
+import image2 from "../assets/dummy-project-2.jpg"
 
 const Projects = () => {
     return(
         <>
             <Header2 title="Projects"/>
             <Grid2x2>
-                
+                <ProjectCard image={image1}/>
+                <ProjectCard image={image2}/>
+                <ProjectCard image={image2}/>
+                <ProjectCard image={image1}/>
             </Grid2x2>
         </>
     )
