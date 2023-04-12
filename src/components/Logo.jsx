41dc9e3879230eb3f16f41ffd5583b20f9806c4d
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-12 12:25:41 -0600

    Created file and added placeholder logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = ({ className }) => {
+    return(
+        <div className={className}>
+            <h1 className="font-Tilt text-3xl">Fer</h1>
+        </div>
+    )
+}
+
+export default Logo
