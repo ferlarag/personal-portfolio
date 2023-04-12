aaddf37f26c089cd60a05d450a59ce84f389a4b1
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-12 12:24:21 -0600

    File created and added placeholders

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,10 @@
+const SocialLinks = ({ className }) => {
+    return(
+        <ul className={className}>
+            <li>1. Linked in</li>
+            <li>2. Github</li>
+        </ul>
+    )
+}
+
+export default SocialLinks
