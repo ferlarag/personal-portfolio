440f534978222392c20402701b5a51d566c2f380
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-11 18:33:09 -0600

    Grid fixes

--- src/components/Grid2x2.jsx
+++ src/components/Grid2x2.jsx
@@ -1,6 +1,9 @@
 const Grid2x2 = ({children}) => {
     return(
-        <div className="grid grid-cols-2 gap-6 mt-8">
+        <div className="
+            grid grid-cols-1s gap-6 mt-8 w-full
+            md:grid-cols-2
+        ">
             {children}
         </div>
     )
