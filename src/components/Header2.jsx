aa95a55fffdc465b481dd6dc9ccc8d9550dae810
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-11 18:35:22 -0600

    Minor responsive design changes

--- src/components/Header2.jsx
+++ src/components/Header2.jsx
@@ -1,6 +1,10 @@
 const Header2 = ({title}) => {
     return(
-        <h2 className="text-[4rem] font-Tilt pt-[140px]">{title}</h2>
+        <h2 className="
+            text-[2.5rem] font-Tilt pt-[70px]
+            md:text-[4rem]
+            sm:pt-[140px]
+        ">{title}</h2>
     )
 }
 
