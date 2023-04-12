264371133143ed65422909ee595d7d3e9657a913
ferlarag <89590031+ferlarag@users.noreply.github.com>
2023-04-12 12:23:11 -0600

    Added elements and animations

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,11 @@
 import { HamburgerMenuIcon } from "@radix-ui/react-icons"
 import { motion } from "framer-motion"
 import { useEffect, useState } from "react"
+import { scroller } from 'react-scroll';
 
 import Button from "./Button"
+import Logo from "./Logo"
+import SocialLinks from "./SocialLinks"
 
 const Nav = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -45,39 +48,55 @@ const Nav = () => {
         setIsOpen(!isOpen)
     }
 
+    const scrollToContact = () => {
+        handleClick()
+        scroller.scrollTo('contact', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+        })
+    }      
+
     return(
         <nav 
             className="
-                flex flex-col p-0 fixed top-0 z-[999] w-full border-b
-                sm:py-2 sm:bg-white
+                flex flex-col p-0 fixed top-0 z-[999] w-full border-b h-14 bg-white justify-center mx-autos
+                sm:py-2 sm:h-auto sm:flex-row sm:px-6 sm:justify-between
             "
         >
-             {/* bg-white bg-opacity-25 backdrop-blur 👆🏽 */}
+            {viewporWidth > 640 && <SocialLinks className="flex flex-row gap-8 items-center"/> }
             <div className="
-                bg-white w-full flex justify-end
+                bg-white w-full flex justify-between items-center
                 sm:hidden sm:bg-transparent
             ">
-                <button className="p-4" onClick={handleClick}>
+                {viewporWidth < 640 && <Logo className="ml-2"/> }
+                <button className="p-2 m-2 border rounded-lg" onClick={handleClick}>
                     <HamburgerMenuIcon />
                 </button>
             </div>
+            {viewporWidth > 640 && <Logo /> }
             <motion.div 
                 className="
-                    max-w-[1160px] w-full mx-auto h-screen flex-grow bg-white pt-10 absolute top-8 z-[-1] left-[100vw]
-                    sm:px-6 sm:h-auto sm:bg-transparent sm:w-full sm:relative sm:top-0 sm:pt-0
+                    max-w-[1160px] w-full h-screen flex-grow bg-white pt-10 absolute top-14 z-[-1] left-[100vw] border-l
+                    sm:h-auto sm:bg-transparent sm:w-auto sm:relative sm:top-0 sm:pt-0 sm:border-l-0 sm:flex-grow-0
                 "
                 animate={{ x: !isOpen && viewporWidth < 639 ? 0 : -viewporWidth}}
-                transition={{duration: 0.5}}
+                // animate only in the mobile version
+                transition={{duration: viewporWidth > 640 ? 0 : 0.5}}
             >
                 <ul className="
-                    flex flex-col items-center gap-4 justify-end
+                    flex flex-col items-center gap-4
                     sm:flex-row
                 ">
                     <li>home</li>
                     <li>project</li>
                     <li>services</li>
-                    <li><Button label="contact" type="main"/></li>
+                    <li><Button label="contact" type="main" onClick={scrollToContact}/></li>
+
+                    {viewporWidth < 640 && <li><SocialLinks className="flex gap-2 border rounded-lg p-4"/></li>}
                 </ul>
+
+                
             </motion.div>
         </nav>
     )
